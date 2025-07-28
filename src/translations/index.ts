6f45e46beac46e9fed8fd866c03cae99bd59d8ba
Eleanor Pitcher <eleanorpitcher@gmail.com>
2025-07-28 21:10:39 +0100

    feat(navigation-menu): translation + layout for nav menu

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,8 +1,11 @@
+import { navigationMenuTranslations } from '@/translations/navigation_menu_translations'
 import { testTranslations } from '@/translations/test_translations'
 import type { TranslationObject } from '@/translations/types'
 
 export const translations: TranslationObject = {
   ...testTranslations,
+  ...navigationMenuTranslations
 }
 
 export type { Language } from '@/translations/types'
+
