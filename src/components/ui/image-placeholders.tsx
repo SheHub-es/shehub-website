c59507a2a93d8fcd0bd97776f8156434cf276bca
Eleanor Pitcher <eleanorpitcher@gmail.com>
2025-08-01 13:45:48 +0100

    feat(image-placeholders): added styling for when no image url is provided

--- src/components/ui/image-placeholders.tsx
+++ src/components/ui/image-placeholders.tsx
@@ -1,14 +1,20 @@
 /* 
-Example Implementation:
+Example Implementation 1:
 
     <ImagePlaceholder 
         size="lg" 
         corner="bottomRight" 
         imageUrl='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d' 
     />
+
+Example Implementation 2:
+
+    <ImagePlaceholder size="sm" corner="topRight" loading />
+
 */
 
 import clsx from "clsx";
+import { Image } from 'lucide-react';
 
 export type ImageSize = "sm" | "md" | "lg";
 export type ImageCorner = "noCorner" | "topLeft" | "topRight" | "bottomLeft" | "bottomRight"
@@ -16,7 +22,8 @@ export type ImageCorner = "noCorner" | "topLeft" | "topRight" | "bottomLeft" | "
 interface ImagePlaceholderProps {
     size: ImageSize;
     corner: ImageCorner;
-    imageUrl: string;
+    imageUrl?: string;
+    loading?: boolean;
 }
 
 const sizeClasses: Record<ImageSize, string> = {
@@ -33,27 +40,44 @@ const cornerClasses: Record<ImageCorner, string> = {
     bottomRight: 'rounded-tl-[50px] rounded-tr-[50px] rounded-br-[4px] rounded-bl-[50px]'
 };
 
+const iconSizes: Record<ImageSize, string> = {
+    sm: 'w-[70px] h-[70px]',
+    md: 'w-[90px] h-[90px]',
+    lg: 'w-[120px] h-[120px]'
+}
+
 export const ImagePlaceholder = ({
     size,
     corner,
-    imageUrl
+    imageUrl,
+    loading
 }: ImagePlaceholderProps) => {
-  
- const commonStyles = clsx(
-    'pt-[12px] pb-[12px] pl-[12px] pr-[12px] gap-[10px] overflow-hidden',
-    sizeClasses[size],
-    cornerClasses[corner]
-)
 
-const imageRounding = cornerClasses[corner] 
+    const commonStyles = clsx(
+        'overflow-hidden hover:ring-[4px] hover:ring-[var(--color-purple-300)]',
+        sizeClasses[size],
+        cornerClasses[corner],
+        iconSizes[size],
+        loading && 'bg-[var(--color-image-container-bg)]'
+    )
+    const imageRounding = cornerClasses[corner] 
 
 return (
     <div className={clsx(commonStyles)}>
-        <img
-            src={imageUrl}
-            alt="image-placeholder"
-            className={clsx('object-cover w-full h-full', imageRounding)}
-        />
+        {imageUrl ? 
+            <img
+                src={imageUrl}
+                alt="image-placeholder"
+                className={clsx('object-cover w-full h-full', imageRounding)}
+            />
+        : (
+            <div className={clsx('flex items-center justify-center w-full h-full', imageRounding)}>
+                <Image
+                    className={clsx(iconSizes[size], 'text-[var(--color-image-stroke-icon)]')}
+                />
+            </div>
+        )}
+        
     </div>
     )
 }
