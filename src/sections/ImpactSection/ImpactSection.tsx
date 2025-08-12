de57636d73902a6b181987b122ff21e4a69bbe90
Rebeca Garcia <rebe.ga87@gmail.com>
2025-08-13 01:59:30 +0200

    feat(impact): build Our Impact So Far section

--- src/sections/ImpactSection/ImpactSection.tsx
+++ src/sections/ImpactSection/ImpactSection.tsx
@@ -1,9 +1,50 @@
 import SectionWrapper from '@/components/layout/section-wrapper/SectionWrapper'
+import { Card } from '@/components/ui/card'
+
+const impactProps = [
+  {
+    title: "100+",
+    description:
+      "women joined the community in our first month"
+  },
+  {
+    title: "3+",
+    description:
+      "active teams working across design, dev, product, and more"
+  },
+  {
+    title: "760+",
+    description:
+      "hours invested by contributors in real workflows"
+  },
+  {
+    title: "5+",
+    description:
+      "interviews landed by contributors in our first month"
+  }
+]
 
 export const ImpactSection = () => {
   return (
-    <SectionWrapper id="about" className='gap-14 grid min-h-[584px] text-black'>
-        <h2>Impact Section</h2>
+    <SectionWrapper id="our-impact" className='flex flex-col py-24 gap-16 bg-primary'>
+      <div  className='flex flex-col justify-center items-center gap-4' >
+                <h2 className='text-white text-size-900 font-bold font-primary '>Our Impact So Far</h2>
+                <p className='font-secondary text-size-500 leading-line-height-body-1 text-white'>
+                SheHub is already helping women build confidence, portfolios and real experience.
+                </p>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-[1.94rem] justify-center'>
+      {impactProps.map((item, index) => (
+        <Card
+          key={index}
+          type='nonClickable'
+          title={item.title}
+          description={item.description}
+          color='white'
+          radius='lg'
+        />
+      ))}
+      </div>
     </SectionWrapper>
   )
 }
