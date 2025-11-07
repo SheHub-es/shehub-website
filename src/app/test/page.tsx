6938dbc0f95fca52b400a064220d96c45a273fe1
Rebeca Garcia <rebe.ga87@gmail.com>
2025-11-07 13:53:45 +0100

    add finished components into feat/test-page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { TextArea } from "@/components/ui/text-area";
 import { Timeline } from "@/components/ui/timeline";
 import { mentorsTimelineData } from "@/data/timeline/mentorsTimelineData";
+import Checkbox from "@/components/ui/checkbox";
 
 export default function TestPage() {
   return (
@@ -12,14 +13,25 @@ export default function TestPage() {
         Components Demo
       </h1>
 
-      <div className="gap-5 justify-center items-center">
+      <div className="gap-10 justify-center items-center">
         <section>
           <h2 className="text-xl font-medium text-purple-700 mb-4">Text Area</h2>
           <TextArea label="Comment" placeholder="Type something..." helperText="Helper Text" />
         </section>
         <section>
-          <h2 className="text-xl font-medium text-purple-700 mb-4 text-center">Timeline</h2>
+          <h2 className="text-xl font-medium text-purple-700 m-4">Checkbox</h2>
+          <Checkbox label="Checkbox" />
+          <Checkbox />
+        </section>
+        <section>
+          <h2 className="text-xl font-medium text-purple-700 mb-4 text-center">Timeline alternate (default)</h2>
           <Timeline items={mentorsTimelineData} />
+          <h2 className="text-xl font-medium text-purple-700 mb-4 text-center">Timeline opposite</h2>
+          <Timeline items={mentorsTimelineData} variant={"opposite"} />
+          <h2 className="text-xl font-medium text-purple-700 mb-4 text-center">Timeline left</h2>
+          <Timeline items={mentorsTimelineData} variant={"left"} />
+          <h2 className="text-xl font-medium text-purple-700 mb-4 text-center">Timeline right</h2>
+          <Timeline items={mentorsTimelineData} variant={"right"} />
         </section>
       </div>
     </main>
