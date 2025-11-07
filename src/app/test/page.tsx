b5e3bbd5e4092d2af5ebb9259cc761ee703d25b9
Rebeca Garcia <rebe.ga87@gmail.com>
2025-11-07 14:08:48 +0100

    add input component into feat/test-page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,6 +5,8 @@ import { TextArea } from "@/components/ui/text-area";
 import { Timeline } from "@/components/ui/timeline";
 import { mentorsTimelineData } from "@/data/timeline/mentorsTimelineData";
 import Checkbox from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Mail } from "lucide-react";
 
 export default function TestPage() {
   return (
@@ -13,13 +15,21 @@ export default function TestPage() {
         Components Demo
       </h1>
 
-      <div className="gap-10 justify-center items-center">
+      <div className="gap-10 flex flex-col items-center">
         <section>
           <h2 className="text-xl font-medium text-purple-700 mb-4">Text Area</h2>
           <TextArea label="Comment" placeholder="Type something..." helperText="Helper Text" />
         </section>
         <section>
-          <h2 className="text-xl font-medium text-purple-700 m-4">Checkbox</h2>
+          <h2 className="text-xl font-medium text-purple-700 mb-4">Input default</h2>
+          <Input label="Email address" required helperText="Please enter a email" icon={Mail} iconAriaLabel="Email icon"/>
+          <h2 className="text-xl font-medium text-purple-700 mb-4">Input success</h2>
+          <Input label="Email address" required status="success" helperText="Correct Email" icon={Mail} iconAriaLabel="Email icon"/>
+          <h2 className="text-xl font-medium text-purple-700 mb-4">Input error</h2>
+          <Input label="Email address" required status="error" helperText="Please enter a valid email" icon={Mail} iconAriaLabel="Email icon"/>
+        </section>
+        <section>
+          <h2 className="text-xl font-medium text-purple-700 mb-4">Checkbox</h2>
           <Checkbox label="Checkbox" />
           <Checkbox />
         </section>
