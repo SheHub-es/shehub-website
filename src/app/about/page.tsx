bb5e2afee550dd9dadd1005a7e4fa56e5de7eb9c
jess-ar <jessica18_24@hotmail.com>
2025-11-12 00:11:45 +0100

    feat(pages): add new pages (Collaborators, Mentors, Partners, About, Contact) and create reusable HeroLayout component with initial design

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import AboutHero from "@/sections/about/hero/AboutHero"
+
+export default function AboutPage() {
+  return (
+    <main>
+      <AboutHero />
+    </main>
+  )
+}
---
+++ src/app/collaborators/page.tsx
@@ -0,0 +1,9 @@
+import CollaboratorsHero from "@/sections/collaborators/hero/CollaboratorsHero"
+
+export default function CollaboratorsPage() {
+  return (
+    <main>
+      <CollaboratorsHero />
+    </main>
+  )
+}
