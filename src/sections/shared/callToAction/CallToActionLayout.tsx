683a2efeae5e5dfe2b7e9c678df120e5e17ae962
jess-ar <jessica18_24@hotmail.com>
2025-11-14 19:18:14 +0100

    refactor(cta): add call-to-action layout and make CTA responsive

---
+++ src/sections/shared/callToAction/CallToActionLayout.tsx
@@ -0,0 +1,55 @@
+import Button from "@/components/ui/Button"
+import React from "react"
+import Link from "next/link"
+
+type ButtonVariant = "secondary-primary"
+
+interface CTAProps {
+  title: string | React.ReactNode
+  buttonText: string
+  buttonHref?: string
+  buttonVariant?: ButtonVariant
+  className?: string
+}
+
+export const CallToActionLayout = ({
+  title,
+  buttonText,
+  buttonHref = "/",
+  buttonVariant = "secondary-primary",
+  className,
+}: CTAProps) => {
+  return (
+    <div
+      id="cta"
+      className={`
+        w-full
+        flex flex-col
+        items-center
+        justify-center
+        text-white
+        gap-8
+        py-28
+        px-4
+        bg-[linear-gradient(90deg,_#f76702_0%,_#f83c85_50%,_#7858ff_100%)]
+        ${className || ""}
+      `}
+    >
+      <h2 className="font-[var(--font-weight-heavy)] text-size-800 md:text-size-900 leading-[var(--spacing-line-height-heading-2)] text-center pb-12">
+        {title}
+      </h2>
+
+      <Link href={buttonHref}>
+        <Button
+          variant={buttonVariant}
+          shape="rounded"
+          className="w-52 h-10 !text-black"
+        >
+          {buttonText}
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+export default CallToActionLayout
