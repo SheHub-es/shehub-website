68d1187d5901724040c89de3205ea45f20d9d2ee
jess-ar <jessica18_24@hotmail.com>
2025-11-14 20:10:58 +0100

    refactor(HowSheHubWorks): improve How SheHub works section and add full mobile responsiveness

--- src/sections/home/howSheHubWorks/HowSheHubWorks.tsx
+++ src/sections/home/howSheHubWorks/HowSheHubWorks.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/ui/Button";
 import { CalendarCheck, FilePenLine, HeartHandshake, UserPlus } from "lucide-react";
+import SectionWrapper from "@/sections/shared/sectionWrapper/SectionWrapper";
 
 export default function HowSheHubWorks() {
   const steps = [
@@ -26,31 +27,26 @@ export default function HowSheHubWorks() {
   ];
 
   return (
-    <section className="text-black bg-background-footer py-24 px-6 font-primary">
-
+    <SectionWrapper className="text-black bg-background-footer py-24 font-primary">
       <div className="text-center mb-16">
-        <p className="text-sm font-bold text-[var(--color-black-text)]">How SheHub works</p>
+        <p className="text-sm md:text-lg font-bold text-[var(--color-black-text)]">How SheHub works</p>
 
-        <h2 className="mt-4 text-4xl md:text-5xl font-bold text-[var(--color-black-text)] max-w-4xl mx-auto leading-[1.2] whitespace-pre-line">
+        <h2 className="mt-4 text-size-800 md:text-size-900 font-bold tracking-tight leading-[1.2] whitespace-pre-line">
           Join, connect, and start building{"\n"}
-          real experience <span className="text-[var(--color-primary-primary,#7858FF)]">in just 4 steps</span>
+          real experience <span className="text-primary">in just 4 steps</span>
         </h2>
-
       </div>
 
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 justify-items-center px-2">
         {steps.map((step, idx) => (
-          <div key={idx} className="flex items-start gap-5 w-[282px]">
+          <div key={idx} className="flex items-start gap-5 w-71">
           
-            <step.icon className="w-[44px] h-[44px] text-[var(--color-black-text)] flex-shrink-0" />
-
-           
+            <step.icon className="w-11 h-11 text-[var(--color-black-text)]" />
             <div className="flex flex-col gap-2">
               <h3 className="text-heading-400 font-bold text-[var(--color-black-text)]">
                 {step.title}
               </h3>
-              <p className="text-body-300 text-[var(--color-black-text)] w-[214px]">
+              <p className="text-body-300 text-[var(--color-black-text)] w-54">
                 {step.desc}
               </p>
             </div>
@@ -66,7 +62,6 @@ export default function HowSheHubWorks() {
           Join a real project
         </Button>
       </div>
-    </section>
+    </SectionWrapper>
   );
 }
-
