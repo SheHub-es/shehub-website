1f7c15872694475968f7059173f2a2087f80f2e9
jess-ar <jessica18_24@hotmail.com>
2026-01-12 23:17:44 +0100

    fix(home): make collaborators and mentors section responsive
    - Add responsive layout (flex-col mobile, flex-row desktop)
    - Reorder elements on mobile (image first, text second)
    - Make images and text blocks responsive
    - Always show InfoBlocks in 2 columns
    - Fix spacing and alignment issues

--- src/sections/home/collaboratorsMentors/components/ImageSection.tsx
+++ src/sections/home/collaboratorsMentors/components/ImageSection.tsx
@@ -1,6 +1,6 @@
-import Image, { StaticImageData } from "next/image";
 import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import { cn } from "@/lib/cn";
+import Image, { StaticImageData } from "next/image";
 
 type ImageSectionVariant = "left" | "right";
 
@@ -42,8 +42,8 @@ const floatingImages: FloatingImageProps[] = [
       width: 279,
       height: 339,
       className: variant === 'left' 
-        ? "absolute top-[22px] left-[32px] fade-in" 
-        : "absolute top-[255px] left-[288px] z-10 fade-in",
+        ? "absolute top-[15px] left-[20px] md:top-[22px] md:left-[32px] w-[140px] h-[170px] md:w-[279px] md:h-[339px] fade-in" 
+        : "absolute top-[150px] left-[180px] md:top-[255px] md:left-[288px] w-[140px] h-[170px] md:w-[279px] md:h-[339px] z-10 fade-in",
     },
     {
       src: smallImage,
@@ -52,42 +52,50 @@ const floatingImages: FloatingImageProps[] = [
         : "Small image",
       width: 188,
       height: 188,
-      className: variant === 'left' ? "absolute top-[388px] left-[341px] z-10" : "absolute top-[81px] left-[43px] z-10"
+      className: variant === 'left' 
+        ? "absolute top-[240px] left-[210px] md:top-[388px] md:left-[341px] w-[95px] h-[95px] md:w-[188px] md:h-[188px] z-10" 
+        : "absolute top-[50px] left-[25px] md:top-[81px] md:left-[43px] w-[95px] h-[95px] md:w-[188px] md:h-[188px] z-10"
     },
     {
       src: arrowIcon,
       alt: "Arrow icon",
-      className: variant === 'left' ? "h-[153px] w-[153px] absolute top-[388px] left-[81px] z-10" : "h-[90px] w-[113px] absolute top-[161px] left-[314px] z-10"
+      className: variant === 'left' 
+        ? "h-[75px] w-[75px] md:h-[153px] md:w-[153px] absolute top-[240px] left-[50px] md:top-[388px] md:left-[81px] z-10" 
+        : "h-[45px] w-[55px] md:h-[90px] md:w-[113px] absolute top-[95px] left-[195px] md:top-[161px] md:left-[314px] z-10"
     },
     {
       src: bigIcon,
       alt: "Big icon",
-      className: variant === 'left' ? "w-[85px] h-[85px] absolute top-[150px] left-[374px] z-10" : "w-[94px] h-[130px] absolute top-[410px] left-[81px] z-10"
+      className: variant === 'left' 
+        ? "w-[42px] h-[42px] md:w-[85px] md:h-[85px] absolute top-[90px] left-[235px] md:top-[150px] md:left-[374px] z-10" 
+        : "w-[47px] h-[65px] md:w-[94px] md:h-[130px] absolute top-[250px] left-[50px] md:top-[410px] md:left-[81px] z-10"
     },
     {
       src: smallIcon,
       alt: "Small icon",
       width: 100,
       height: 50,
-      className: variant === 'left' ? "h-[100px] w-[100px] absolute top-[250px] left-[370px] z-10" : "h-[50px] w-[50px] absolute top-[304px] left-[88px] z-10"
+      className: variant === 'left' 
+        ? "h-[50px] w-[50px] md:h-[100px] md:w-[100px] absolute top-[150px] left-[230px] md:top-[250px] md:left-[370px] z-10" 
+        : "h-[25px] w-[25px] md:h-[50px] md:w-[50px] absolute top-[190px] left-[55px] md:top-[304px] md:left-[88px] z-10"
     }
 ]
 
   const [mainImageRef, isMainImageVisible] = useIntersectionObserver();
 
   return (
-    <div className="relative w-[584px] h-[620px] flex m-auto ml-[-20px]">
-      <div className={`absolute flex z-0 w-[327px] h-[331px] ${
+    <div className="relative w-full max-w-[584px] h-auto min-h-[300px] md:min-h-[620px] md:h-[620px] flex m-auto md:ml-line-height-label-2">
+      <div className={`absolute flex z-0 w-[160px] h-[160px] md:w-[327px] md:h-[331px] ${
         variant === "left"
-          ? "top-[192px] left-[148px]"
-          : "top-[181px] left-[113px]"
+          ? "top-[95px] left-line-height-heading-2 md:top-[192px] md:left-[148px]"
+          : "top-[90px] left-[45px] md:top-[181px] md:left-[113px]"
       }`}>
         <Image
           src={centralImage}
           alt="Central image"
           width={300}
           height={300}
-          className=""
+          className="w-full h-full object-contain"
         />
       </div>
 
