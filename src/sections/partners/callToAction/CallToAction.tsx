d4d19cd7f955818db583f4fb5c8aa9cbb9735d33
kat-lev <kat@katleverton.com>
2026-05-26 13:28:33 +0200

    chore(i18n): replace hardcoded strings with t keys on partners page

--- src/sections/partners/callToAction/CallToAction.tsx
+++ src/sections/partners/callToAction/CallToAction.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import CallToActionLayout from "@/sections/shared/callToAction/CallToActionLayout";
+import { useTranslation } from "@/hooks/useTranslation";
 
 export default function PartnersCallToAction() {
+  const { t } = useTranslation();
+
   return (
     <CallToActionLayout
-      title="Want to collaborate?"
-      buttonText="Join as a partner"
+      title={t('partners.cta.title')}
+      buttonText={t('partners.cta.button')}
       buttonHref="/join"
       buttonVariant="secondary-primary"
     />
