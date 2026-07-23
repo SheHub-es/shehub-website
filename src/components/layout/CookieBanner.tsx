af9587dfb7694093eb2774b78d9b6546fb6cfa3f
kat-lev <kat@katleverton.com>
2026-07-23 19:30:58 +0200

    styles(cookie-banner): add lock icon to switch component, adapt layout and update copy

--- src/components/layout/CookieBanner.tsx
+++ src/components/layout/CookieBanner.tsx
@@ -32,12 +32,12 @@ type CookieCategoryRowProps = {
 };
 
 const CookieCategoryRow = ({ title, description, disabled, checked, onChange }: CookieCategoryRowProps) => (
-  <div className="flex items-start justify-between gap-4 py-4">
-    <div className="flex flex-col gap-2">
-      <p className="font-secondary text-size-300 font-heavy leading-line-height-body-3">{title}</p>
-      <p className="font-secondary text-size-300 leading-line-height-body-3 text-foreground">{description}</p>
+  <div className="flex flex-col gap-2 py-4">
+    <div className="flex items-center justify-between gap-4">
+      <p className="font-secondary text-size-300 font-heavy leading-line-height-body-3 text-black">{title}</p>
+      <Switch disabled={disabled} checked={checked} onChange={onChange} ariaLabel={title} />
     </div>
-    <Switch disabled={disabled} checked={checked} onChange={onChange} ariaLabel={title} />
+    <p className="font-secondary text-size-300 leading-line-height-body-3 text-neutral-600">{description}</p>
   </div>
 );
 
@@ -156,7 +156,7 @@ const CookieBanner = () => {
               onClick={() => setIsManagingPreferences(false)}
               className="shrink-0 cursor-pointer rounded-full p-1 hover:bg-neutral-100"
             >
-              <NextImage src={CloseIcon} alt="" width={16} height={16} />
+              <NextImage src={CloseIcon} alt="" width={24} height={24} />
             </button>
           </div>
 
@@ -213,7 +213,7 @@ const CookieBanner = () => {
           </div>
         </div>
 
-        <div className="flex flex-col gap-3 lg:shrink-0 lg:flex-row lg:items-center">
+        <div className="flex flex-col gap-3 lg:shrink-0 lg:flex-row lg:items-center lg:pl-8">
           {isManagingPreferences ? (
             <Button
               ref={managePreferencesButtonRef}
